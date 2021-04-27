5d82b92304f1b16294fec42f008a10317646eb40
kabilash <kabilash143@gmail.com>
2021-04-27 22:55:47 +0530

    footer completed

--- index.php
+++ index.php
@@ -226,4 +226,50 @@
     </div>
   </div>
 </section>
+
+<footer class="footer section-padding-y">
+  <div class="col-11 mx-auto">
+    <div class="row">
+      <div class="col-md-4 text-center">
+        <h2>History</h2>
+      </div>
+      <div class="col-md-4 text-center">
+        <h2>Culprit Rules</h2>
+      </div>
+      <div class="col-md-4 text-center">
+        <h2>Complaints</h2>
+      </div>
+    </div>
+
+    <ul class="social">
+      <li>
+        <a href="javascript:void(0);">
+          <img src="https://place-hold.it/25" alt="">
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0);">
+          <img src="https://place-hold.it/25" alt="">
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0);">
+          <img src="https://place-hold.it/25" alt="">
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0);">
+          <img src="https://place-hold.it/25" alt="">
+        </a>
+      </li>
+      <li>
+        <a href="javascript:void(0);">
+          <img src="https://place-hold.it/25" alt="">
+        </a>
+      </li>
+    </ul>
+
+    <span class="copyright">&copy; All Rights Reserved</span>
+  </div>
+</footer>
 <?php include "includes/footer-assets.php"; ?>
